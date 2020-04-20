abd1c7b0b64e61dee69841987ef1180fa9870d37
Vinnie <vz377@nyu.edu>
2020-04-20 01:12:06 -0400

    added lighthouse

--- js/sunset.js
+++ js/sunset.js
@@ -2,10 +2,17 @@
 let cloud = document.getElementById("cloud");
 let birds = document.getElementById("birds");
 
+let lighthouse = document.getElementById("lh");
+let pulse = document.getElementById("pulse");
 
 cloud.addEventListener("mouseover", function(){
     birds.style.display = "initial";
 
     setTimeout(function(){ birds.style.display = "none"; }, 5000); // makes birds disappear
+});
 
-  });
+lighthouse.addEventListener("mouseover", function(){
+    pulse.style.display = "initial";
+
+    setTimeout(function(){ pulse.style.display = "none"; }, 4700); // makes light disappear
+});
