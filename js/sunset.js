7258882c59ded535a5fec96bda1743ff22248257
Vinnie <vz377@nyu.edu>
2020-04-21 19:59:36 -0400

    swapped and added some assets, added more animation/audio

--- js/sunset.js
+++ js/sunset.js
@@ -1,18 +1,22 @@
 
 let cloud = document.getElementById("cloud");
 let birds = document.getElementById("birds");
+let chirp = document.getElementById("chirp");
 
 let lighthouse = document.getElementById("lh");
 let pulse = document.getElementById("pulse");
+let ramen = document.getElementById("ramen");
 
 cloud.addEventListener("mouseover", function(){
     birds.style.display = "initial";
+    chirp.play();
 
     setTimeout(function(){ birds.style.display = "none"; }, 5000); // makes birds disappear
 });
 
 lighthouse.addEventListener("mouseover", function(){
     pulse.style.display = "initial";
+    ramen.style.display = "initial";
 
-    setTimeout(function(){ pulse.style.display = "none"; }, 4700); // makes light disappear
+    setTimeout(function(){ pulse.style.display = "none"; ramen.style.display = "none"; }, 5000); // makes light disappear
 });
